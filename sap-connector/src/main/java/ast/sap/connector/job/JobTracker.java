1ec5f3fd64bd5d7de38c0423d931c911f9c3cb01
zaragozamartin91 <zaragozamartin91@gmail.com>
2017-07-18 16:06:55 -0300

    Se modifico el tracker de jobs

--- sap-connector/src/main/java/ast/sap/connector/job/JobTracker.java
+++ sap-connector/src/main/java/ast/sap/connector/job/JobTracker.java
@@ -1,7 +1,16 @@
 package ast.sap.connector.job;
 
 import ast.sap.connector.dst.SapRepository;
+import ast.sap.connector.func.SapFunction;
+import ast.sap.connector.func.SapFunctionResult;
+import ast.sap.connector.func.SapStruct;
 
+/**
+ * Monitor de status de jobs.
+ * 
+ * @author martin
+ *
+ */
 public class JobTracker {
 	private final SapRepository sapRepository;
 
@@ -9,8 +18,22 @@ public class JobTracker {
 		this.sapRepository = sapRepository;
 	}
 
-	// TODO
+	/**
+	 * Obtiene el estado de un job.
+	 * 
+	 * @param jobData - Informacion del job a monitorear.
+	 * @return Estado del job.
+	 */
 	public JobStatus getStatus(TrackerJobData jobData) {
-		return new JobStatus(null, null);
+		SapFunction jobStartAsapFunction = sapRepository.getFunction("BAPI_XBP_JOB_STATUS_GET")
+				.setInParameter("JOBNAME", jobData.getJobName())
+				.setInParameter("JOBCOUNT", jobData.getJobId())
+				.setInParameter("EXTERNAL_USER_NAME", jobData.getExternalUsername());
+
+		SapFunctionResult result = jobStartAsapFunction.execute();
+		String status = result.getOutParameterValue("STATUS").toString();
+		SapStruct ret = result.getStructure("RETURN");
+		
+		return new JobStatus(status, ret);
 	}
 }
